30dcddb3ed86a49d7c7033c1e776ae97d27d9625
nick-dietz <64912283+nick-dietz@users.noreply.github.com>
2022-06-10 15:39:10 -0600

    Cleanup Aisle 9

--- RokuV1/RokuV1/App.cs
+++ RokuV1/RokuV1/App.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Roku_SSharp.App
-// Assembly: Roku SSharp, Version=1.0.0.34586, Culture=neutral, PublicKeyToken=null
-// MVID: E4B5925D-8C34-4CFE-B7EA-59A65434D500
-// Assembly location: C:\Users\Nick\Downloads\Roku 3 Series Release v1.1 (1)\Roku 3 Series Release\Roku SSHarp\Roku SSharp.dll
+﻿
 
 namespace Roku_SSharp
 {
